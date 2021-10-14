5e0da77625921bd6497c0566f7f33dbb6613daa6
Jake Donham <jake.donham@gmail.com>
2021-10-13 22:23:24 -0700

    Updates

--- part2/dist/type/isSubtype.js
+++ part2/dist/type/isSubtype.js
@@ -1,7 +1,7 @@
 import * as Trace from "../util/trace.js";
 import propType from "./propType.js";
 import * as Type from "./validators.js";
-const isSubtype = Trace.instrument("isSubtype", function isSubtype2(a, b) {
+const isSubtype = Trace.instrument("isSubtype", function (a, b) {
   if (Type.isNull(a) && Type.isNull(b)) return true;
   if (Type.isBoolean(a) && Type.isBoolean(b)) return true;
   if (Type.isNumber(a) && Type.isNumber(b)) return true;
@@ -13,12 +13,12 @@ const isSubtype = Trace.instrument("isSubtype", function isSubtype2(a, b) {
       type: bType
     }) => {
       const aType = propType(a, bName);
-      if (!aType) return false;else return isSubtype2(aType, bType);
+      if (!aType) return false;else return isSubtype(aType, bType);
     });
   }
 
   if (Type.isFunction(a) && Type.isFunction(b)) {
-    return a.args.length === b.args.length && a.args.every((a2, i) => isSubtype2(b.args[i], a2)) && isSubtype2(a.ret, b.ret);
+    return a.args.length === b.args.length && a.args.every((a2, i) => isSubtype(b.args[i], a2)) && isSubtype(a.ret, b.ret);
   }
 
   return false;
